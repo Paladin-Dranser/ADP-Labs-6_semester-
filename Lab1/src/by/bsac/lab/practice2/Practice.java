20540ff90028bcbf9e1bfd470c9acd589d82d346
Paladin-Dranser <anton.butsko@gmail.com>
2019-02-08 08:53:11 +0300

    Add the 2th and the 3th tasks

--- Lab1/src/by/bsac/lab/practice2/Practice.java
+++ Lab1/src/by/bsac/lab/practice2/Practice.java
@@ -1,4 +1,21 @@
+/**
+ * @autor Antoś Bućko
+ */
 package by.bsac.lab.practice2;
-
+/*
+    Программа, в которой все переданные во входную строку аргументы выводятся на экран в
+    обратной порядке. Например, если было передано 2 аргумента – make install, то на экран должно
+    вывестись llatsni ekam. Примечание*: для разбора слова по буквам необходимо использовать функцию
+    charAt(). Например, str.charAt(i) вернет символ с позиции i в слове, записанном в строковую
+    переменную str. Команда str.length() возвращает длину слова str.
+ */
 public class Practice {
+    public static void main(String[] args) {
+        for (int i = args.length - 1; i >= 0; i--) {
+            for (int j = args[i].length() - 1; j >= 0; j--) {
+                System.out.print(args[i].charAt(j));
+            }
+            System.out.print(' ');
+        }
+    }
 }
---
+++ Lab1/src/by/bsac/lab/practice3/Practice.java
@@ -0,0 +1,42 @@
+/**
+ * @autor Antoś Bućko
+ */
+package by.bsac.lab.practice3;
+
+import java.util.Scanner;
+
+/*
+    Создайте программу, вычисляющую числа Фибоначчи. Числа Фибоначчи –
+    последовательность чисел, в котором каждое следующее число равно сумме двух предыдущих.
+    Начало этой последовательности – числа 1, 1, 2, 3, 5, 8, 13...
+ */
+public class Practice {
+    public static void main(String[] args) {
+        System.out.print("Enter the number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        int previousNumber = 0;
+        int currentNumber = 1;
+
+        for (int i = 0; i < number; i++) {
+            System.out.print(String.valueOf(fibonacci(i)) + ' ');
+        }
+    }
+
+    private static int fibonacci(int id) {
+        /**
+         * @param id - id of number in the Fibonacci sequence
+         * @return Return the Nth number in the Fibonacci sequence
+         * @see Algorithm Implementation/Mathematics/Fibonacci Number Program (wikibooks)
+         */
+        int lo = 0;
+        int hi = 1;
+        for (int i = 0; i < id; i++) {
+            hi = lo + hi;
+            lo = hi - lo;
+        }
+
+        return lo;
+    }
+}
